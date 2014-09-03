d11c5245f260a4485ab62b8ce1f991993be3d6ec
Matt Sharpe <s3481178@student.rmit.edu.au>
2014-09-03 23:51:55 +1000

    scaffold index functions

--- src/common.h
+++ src/common.h
@@ -39,3 +39,9 @@ struct mapping {
 int load_map(FILE *mapfile, struct mapping **map);
 void map_document(FILE *mapfile, char document[13], uint32_t id);
 char *unmap_document(struct mapping **map, uint32_t id);
+
+// Functions for lexicon
+
+// Functions for index
+uint32_t index_token(FILE *index, struct tokenlist *token);
+void index_find(FILE *index, uint32_t offset, struct tokenlist *tokens)
